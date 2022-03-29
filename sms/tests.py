7b63e80581e4ee64b9bbc9416ac90a3029803eee
Rich Hildred <rhildred@gmail.com>
2022-03-28 22:11:58 -0400

    add sample test to main branch

--- sms/tests.py
+++ sms/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from sms.models import Order
 
 # Create your tests here.
+
+class OrderTest(TestCase):
+    def test_welcome(self):
+        oOrder = Order(phone="519-226-1234", data = {"state":"WELCOMING"})
+        aReturn = oOrder.handleInput("hello")
+        print(aReturn)
+        self.assertTrue("welcome" in aReturn[0].lower())
